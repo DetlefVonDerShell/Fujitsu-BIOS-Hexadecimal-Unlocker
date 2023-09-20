8cddb35ac447c85fee5f36a74fdf78c9cc2228b7
DetlefVonDerShell <83460577+DetlefVonDerShell@users.noreply.github.com>
2023-09-20 11:00:42 +0200

    Update main.py

--- main.py
+++ main.py
@@ -3,39 +3,40 @@ import crcmod
 crc32_func = crcmod.mkCrcFun(0x104c11db7, initCrc=0xFFFFFFFF, xorOut=0x00000000)
 
 while True:
-    # Benutzer zur Eingabe auffordern
+    # Prompt User for Input
     input_string = input("Please enter the code: ")
-    # Exit Funktion
+    # Exit Function
     if input_string.lower() == 'exit':
         print("Exiting..")
         break
         #
 
-    # Aufteilen der Eingabe in Blöcke
+    # Divide Input in Blocks
     blocks = input_string.split('-')
 
-    # Überprüfe, ob die Eingabe die richtige Länge hat
+    # Check input length
     if len(blocks) == 6 and len(blocks[0]) <= 4 and len(blocks[1]) <= 4 and len(blocks[2]) <= 4 and len(
             blocks[3]) <= 4 and len(blocks[4]) <= 4 and len(blocks[5]) <= 4:
-        # Überprüfe, ob die ersten beiden Blöcke übereinstimmen
+                
+        # Check first two blocks
         if blocks[0] == "203c" and blocks[1] == "d001":
-            # Verbinde die restlichen Blöcke ohne Bindestriche
+            
+            # Connect blocks
             result_string = "".join(blocks[2:])
-            # Entferne alle Bindestriche aus der Zeichenfolge
+            
+            # Replace - Symbol
             result_string = result_string.replace("-", "")
-            # Ausgabe der Zeichenfolge
+            
+            # Print Statement
             print("Input ok.")
-            # Ändere die Eingabe zu hexadezimal
+            
+            # Change Input to Hex
             hex_result = hex(int(result_string, 16))[2:].encode('utf-8')
             crc_sum = hex(crc32_func(bytearray(hex_result)))
 
-            # Colors for better visability
-            TGREEN = '\033[32m'
-            RESET = '\033[m'
-
-            print("The master password is: ", TGREEN + crc_sum[2:] + RESET)
+            print("The master password is: ", crc_sum[2:])
             print("Please note that the password is encoded for US QWERTY keyboard layouts.")
-            break  # Beende die Schleife, da die Eingabe gültig ist
+            break
         else:
             print("Wrong Input! The first two blocks must be '203c' and 'd001'.")
     else:
